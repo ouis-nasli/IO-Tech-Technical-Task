7ada9eaed56b7e1e17d9df2cfd634d7ab9b662ee
ouis-nsli <ouis.suaeid.nasli@gmail.com>
2024-11-12 21:24:28 +0400

    Clean up

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,32 @@
-import React from 'react';
-import logo from './logo.svg';
+import { QueryClientProvider } from 'react-query';
 import './App.css';
+import AppRouting from './AppRouting';
+
+import queryClient from './queryClient';
+import { ToastContainer } from 'react-toastify';
+
+import 'react-toastify/dist/ReactToastify.css';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+	return (
+		<>
+			<QueryClientProvider client={queryClient}>
+				<AppRouting />
+				<ToastContainer
+					position='bottom-right'
+					autoClose={5000}
+					hideProgressBar={false}
+					newestOnTop
+					closeOnClick
+					rtl={false}
+					pauseOnFocusLoss
+					draggable
+					pauseOnHover
+					theme='light'
+				/>
+			</QueryClientProvider>
+		</>
+	);
 }
 
 export default App;
