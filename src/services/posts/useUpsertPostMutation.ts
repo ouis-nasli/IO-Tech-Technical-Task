51d9c68524ce54f0ad780dfa98fd20a6a2c8f11a
ouis-nsli <ouis.suaeid.nasli@gmail.com>
2024-11-12 21:29:03 +0400

    Create Upsert post service

---
+++ src/services/posts/useUpsertPostMutation.ts
@@ -0,0 +1,17 @@
+import { UseMutationOptions, useMutation } from 'react-query';
+import { HttpError } from 'src/interfaces/common';
+import { _axios } from '../httpConfig';
+import { IUpsertPostMutation } from './interface';
+
+export function useUpsertPostMutation(
+	id?: string | null,
+	options?: UseMutationOptions<boolean, HttpError, IUpsertPostMutation>
+) {
+	return useMutation((params) => (id ? updateFunc(id, params) : createFunc(params)), options);
+}
+
+const updateFunc = (id: string, params: IUpsertPostMutation) =>
+	_axios.patch<boolean>(`/posts/${id}`, params).then((res) => res.data);
+const createFunc = (params: IUpsertPostMutation) => _axios.post<boolean>(`/posts`, params).then((res) => res.data);
+
+export default useUpsertPostMutation;
