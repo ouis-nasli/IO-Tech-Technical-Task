9d61bf87c79bb2907b623c935777d46e2bf3c175
ouis-nsli <ouis.suaeid.nasli@gmail.com>
2024-11-13 04:16:59 +0400

    Update useUpsertPostMutation.ts

--- src/services/posts/useUpsertPostMutation.ts
+++ src/services/posts/useUpsertPostMutation.ts
@@ -4,13 +4,13 @@ import { _axios } from '../httpConfig';
 import { IUpsertPostMutation } from './interface';
 
 export function useUpsertPostMutation(
-	id?: string | null,
+	id?: number | null,
 	options?: UseMutationOptions<boolean, HttpError, IUpsertPostMutation>
 ) {
 	return useMutation((params) => (id ? updateFunc(id, params) : createFunc(params)), options);
 }
 
-const updateFunc = (id: string, params: IUpsertPostMutation) =>
+const updateFunc = (id: number, params: IUpsertPostMutation) =>
 	_axios.patch<boolean>(`/posts/${id}`, params).then((res) => res.data);
 const createFunc = (params: IUpsertPostMutation) => _axios.post<boolean>(`/posts`, params).then((res) => res.data);
 
